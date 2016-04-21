87fee03ffe4896244947b97690090e6d1b326833
Stefan Schlager <zarquon42@gmail.com>
2016-04-21 17:32:20 +0200

    added try..catch

--- src/test.cpp
+++ src/test.cpp
@@ -3,13 +3,13 @@
 extern "C" {
   extern SEXP R_swig_Image_GetDimension ( SEXP self, SEXP s_swig_copy);
   SEXP Image_GetDimension(SEXP image) {
-    // try {
+     try {
       SEXP out = R_swig_Image_GetDimension(image, Rcpp::wrap(true));
       return out;
-      // } catch (std::exception& e) {
-      // ::Rf_error( e.what());
-      // } catch (...) {
-      //::Rf_error("unknown exception");
-      //}
+       } catch (std::exception& e) {
+       ::Rf_error( e.what());
+       } catch (...) {
+      ::Rf_error("unknown exception");
+      }
   }
 }
